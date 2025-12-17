acc606634d9ce7c392321eea28c00a43443c8ca5
Ian <ianfogelman@gmail.com>
2025-12-16 21:14:06 -0500

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/bad-experience-dot-io/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
